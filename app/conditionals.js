eaf8ae7ce9bb1da6f50d000929761c960e4098dc
Troy Fuhriman <TkFuhriman@gmail.com>
2020-05-05 11:58:24 -0600

    almost done

--- app/conditionals.js
+++ app/conditionals.js
@@ -1,7 +1,7 @@
 //1. Given two numbers, write a function that will return  the larger number
 
 function largerNum(num1, num2) {
-    // you code here
+    return Math.max(num1, num2)
 
 }
 
@@ -14,7 +14,11 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+    var grade = score / possible;
+    if (grade >= .90) { output = "A" }
+    else if (grade >= .80) { output = "B" }
+    else if (grade >= .70) { output = "C" }
+    return output
 }
 
 
@@ -29,13 +33,18 @@ function testGrader(score, possible) {
 //Make sure your ranges are inclusive
 
 function timeOfDayGreeting(hour) {
-    // you code below
-
+    if (hour >= 5 && hour <= 11) { return "good morning" }
+    else if (hour >= 12 && hour <= 17) { return "good afternoon" }
+    else if (hour >= 18 && hour <= 21) { return "good evening" }
+    else if (hour >= 22 && hour <= 24) { return "good night" }
+    else if (hour >= 1 && hour <= 4) { return "good night" }
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+    if (temp <= 98.6) { return "no fever" }
+    else if (temp >= 98.7 && temp <= 102.9) { return "fever" }
+    else if (temp >= 103) { return "fever go to hospital" }
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -47,7 +56,8 @@ let myCar = {
 }
 
 function isStopped(car) {
-
+    if (car.moving == false) { return true }
+    else if (car.moving == true) { return false }
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
@@ -58,5 +68,6 @@ let dish = {
 }
 
 function washDish(dish) {
-
+    if (dish.yourDish == true && dish.isDirty == true) { return true }
+    else return false
 }
